ca68b37dcf85c25ba85e0ff4cf715d714067c6e6
TrentDev <TrentLathim@gmail.com>
2022-06-16 01:42:28 -0700

    created project

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+// Package that adds helper functions to my code
+#include <iostream>
+
+
+using single_number = int;
+// Start of the actual program
+int main() {
+    std::string playerOne;
+    std::string playerTwo;
+    const int THREE_POINTS_TO_WIN = 3;
+
+    single_number dayOfWeek = 4;
+    std::cout << dayOfWeek;
+    // If return 0 is reached then the program has successfully run
+    return 0; 
+}
